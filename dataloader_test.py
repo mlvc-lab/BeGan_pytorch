7fb5c1b64f7a5cc8b1a05b1f30f3b658c6e81410
EunSeop Shin <kairos9603@gmail.com>
2018-07-31 13:54:50 +0900

    Add more dataloader test
    BeGan_base#1

--- dataloader_test.py
+++ dataloader_test.py
@@ -16,10 +16,22 @@ class DataLoaderTest(unittest.TestCase):
             image.save("test/HMI0100/test_img{}.jpg".format(i), "JPEG")
 
     def test_dataloader(self):
-        dl = get_loader("test/", batch_size=3, scale_size=64, seq_size=3, num_workers=1)
+        batch_size = 3
+        scale_size = 64
+        seq_size = 3
+
+        dl = get_loader("test/", batch_size=batch_size, scale_size=scale_size, seq_size=seq_size, num_workers=1)
 
         for idx, (inputs, targets) in enumerate(dl):
-            print(idx, (inputs.shape, targets.shape))
+            # print(idx, (inputs.shape, targets.shape))
+            # output shape check
+            self.assertEqual(tuple(inputs.shape), (batch_size, seq_size, 1, scale_size, scale_size)) 
+            self.assertEqual(tuple(targets.shape), (batch_size, seq_size, 1, scale_size, scale_size))
+
+        # image size and target size check
+        self.assertEqual(len(dl.dataset.input_images), len(dl.dataset.target_images))
+        # iteration size check
+        self.assertEqual(len(dl.dataset.input_images) // dl.dataset.seqSize, idx+1)
 
     def tearDown(self):
         for i in range(50):
